89ee21c6d03303f16616303fe97444452d2e790b
Oleksii Fedorov <waterlink000@gmail.com>
2016-03-01 01:55:02 +0100

    Implement TicTacToe::Game

--- spec/game_spec.cr
+++ spec/game_spec.cr
@@ -1,23 +1,415 @@
 require "./spec_helper"
 require "../src/game"
 
+macro game_over(g1, g2, g3, over)
+  {% game = [g1, g2, g3] %}
+
+  expect(Game.from({{game.argify}}).over?)
+    .to eq({{over}})
+end
+
+macro game(g1, g2, g3, *expectations)
+  {% game = [g1, g2, g3] %}
+  game = Game.from({{game.argify}})
+
+  {% for expectation in expectations %}
+    {{expectation}}
+  {% end %}
+end
+
+macro transform(g1, g2, g3, transformation, e1, e2, e3, expectation = nil)
+  {% game = [g1, g2, g3] %}
+  {% expected = [e1, e2, e3] %}
+
+  game = Game.from({{game.argify}})
+  %expected = Game.from({{expected.argify}})
+  game = {{transformation}}
+
+  expect(game).to eq(%expected)
+  {{expectation}}
+end
+
 module TicTacToe
   Spec2.describe Game do
-    let(empty) { [] of {Int32, Int32} }
+    let(empty) { [] of Game::Field }
 
-    it "is over when all fields are taken" do
-      # xxo
-      # oox
-      # xox
-      game = Game.new(
-        fields: empty,
-        first: [{0, 0}, {0, 1}, {2, 0}, {1, 2}, {2, 2}],
-        second: [{1, 1}, {0, 2}, {1, 0}, {2, 1}],
+    it "is possible to create game state with nice macro" do
+      expect(
+        Game.from(
+          xxo,
+          o_x,
+          o__,
+        )
+      ).to eq(
+        Game.new(
+          fields: [{1, 1}, {2, 1}, {2, 2}],
+          first: [{0, 0}, {0, 1}, {1, 2}],
+          second: [{0, 2}, {1, 0}, {2, 0}],
+        )
       )
 
-      expect(game.over?).to eq(true)
+      expect(
+        Game.from(
+          xxo,
+          oox,
+          xox,
+        )
+      ).to eq(
+        Game.new(
+          fields: empty,
+          first: [{0, 0}, {0, 1}, {1, 2}, {2, 0}, {2, 2}],
+          second: [{0, 2}, {1, 0}, {1, 1}, {2, 1}],
+        )
+      )
     end
 
-    # TODO: write more tests
+    it "is over when all fields are taken" do
+      game_over(
+        xxo,
+        oox,
+        xox,
+        true
+      )
+    end
+
+    it "is not over when there are some fields left" do
+      game_over(
+        xxo,
+        oox,
+        xo_,
+        false,
+      )
+    end
+
+    it "is over when all fields in a column are taken by a player" do
+      game_over(
+        x_o,
+        xo_,
+        x__,
+        true,
+      )
+
+      game_over(
+        _xo,
+        ox_,
+        _x_,
+        true,
+      )
+
+      game_over(
+        _ox,
+        o_x,
+        __x,
+        true,
+      )
+
+      game_over(
+        o_x,
+        ox_,
+        o__,
+        true,
+      )
+
+      game_over(
+        _ox,
+        xo_,
+        _o_,
+        true,
+      )
+
+      game_over(
+        _xo,
+        x_o,
+        __o,
+        true,
+      )
+    end
+
+    it "is over when all fields in a row are taken by a player" do
+      game_over(
+        xxx,
+        o_o,
+        ___,
+        true,
+      )
+
+      game_over(
+        o_o,
+        xxx,
+        ___,
+        true,
+      )
+
+      game_over(
+        o_o,
+        ___,
+        xxx,
+        true,
+      )
+
+      game_over(
+        ooo,
+        x_x,
+        ___,
+        true,
+      )
+
+      game_over(
+        x_x,
+        ooo,
+        ___,
+        true,
+      )
+
+      game_over(
+        x_x,
+        ___,
+        ooo,
+        true,
+      )
+    end
+
+    it "is over when all fields in a diagonal are taken by a player" do
+      game_over(
+        x_o,
+        _xo,
+        __x,
+        true,
+      )
+
+      game_over(
+        o_x,
+        ox_,
+        x__,
+        true,
+      )
+
+      game_over(
+        o_x,
+        _ox,
+        __o,
+        true,
+      )
+
+      game_over(
+        x_o,
+        xo_,
+        o__,
+        true,
+      )
+    end
+
+    describe "a player" do
+      it "can take a field if it is not taken" do
+        transform(
+          ___,
+          ___,
+          ___,
+
+          game.take({0, 0}),
+
+          x__,
+          ___,
+          ___,
+
+          expect(game.notice).to eq(""),
+        )
+
+        transform(
+          ___,
+          ___,
+          ___,
+
+          game.take({1, 2}),
+
+          ___,
+          __x,
+          ___,
+
+          expect(game.notice).to eq(""),
+        )
+
+        transform(
+          ___,
+          ___,
+          _x_,
+
+          game.take({1, 2}),
+
+          ___,
+          __o,
+          _x_,
+
+          expect(game.notice).to eq(""),
+        )
+
+        transform(
+          ___,
+          o__,
+          _x_,
+
+          game.take({1, 2}),
+
+          ___,
+          o_x,
+          _x_,
+
+          expect(game.notice).to eq(""),
+        )
+
+        transform(
+          ___,
+          o_x,
+          _x_,
+
+          game.take({1, 1}),
+
+          ___,
+          oox,
+          _x_,
+
+          expect(game.notice).to eq(""),
+        )
+      end
+
+      it "is not possible to take already taken field" do
+        transform(
+          ___,
+          o_x,
+          _x_,
+
+          game.take({1, 0}),
+
+          ___,
+          o_x,
+          _x_,
+
+          expect(game.notice)
+            .to eq("Field {1, 0} is already taken"),
+        )
+      end
+
+      it "is not possible to take field when game is over" do
+        transform(
+          _x_,
+          ooo,
+          x_x,
+
+          game.take({0, 2}),
+
+          _x_,
+          ooo,
+          x_x,
+
+          expect(game.notice)
+            .to eq("Game over. Second player won."),
+        )
+      end
+    end
+
+    describe "#notice & #outcome" do
+      it "shows game over and outcome message" do
+        game(
+          xxx,
+          ___,
+          o_o,
+
+          expect(game.outcome).to(eq :first_won),
+          expect(game.notice)
+            .to eq("Game over. First player won."),
+        )
+
+        game(
+          _x_,
+          _x_,
+          oxo,
+
+          expect(game.outcome).to(eq :first_won),
+          expect(game.notice)
+            .to eq("Game over. First player won."),
+        )
+
+        game(
+          xo_,
+          _x_,
+          o_x,
+
+          expect(game.outcome).to(eq :first_won),
+          expect(game.notice)
+            .to eq("Game over. First player won."),
+        )
+
+        game(
+          _ox,
+          _x_,
+          x_o,
+
+          expect(game.outcome).to(eq :first_won),
+          expect(game.notice)
+            .to eq("Game over. First player won."),
+        )
+
+        game(
+          x_x,
+          ___,
+          ooo,
+
+          expect(game.outcome).to(eq :second_won),
+          expect(game.notice)
+            .to eq("Game over. Second player won."),
+        )
+
+        game(
+          _o_,
+          _o_,
+          xox,
+
+          expect(game.outcome).to(eq :second_won),
+          expect(game.notice)
+            .to eq("Game over. Second player won."),
+        )
+
+        game(
+          ox_,
+          _o_,
+          x_o,
+
+          expect(game.outcome).to(eq :second_won),
+          expect(game.notice)
+            .to eq("Game over. Second player won."),
+        )
+
+        game(
+          _xo,
+          _o_,
+          o_x,
+
+          expect(game.outcome).to(eq :second_won),
+          expect(game.notice)
+            .to eq("Game over. Second player won."),
+        )
+
+        game(
+          oxx,
+          xoo,
+          xox,
+
+          expect(game.outcome).to(eq :draw),
+          expect(game.notice)
+            .to eq("Game over. Draw."),
+        )
+      end
+
+      it "does not show outcome when game is ongoing" do
+        game(
+          ___,
+          _x_,
+          o__,
+
+          expect(game.outcome).to(eq :ongoing),
+          expect(game.notice).to eq(""),
+        )
+      end
+    end
   end
 end
--- src/game.cr
+++ src/game.cr
@@ -1,13 +1,192 @@
 module TicTacToe
   class Game
-    def initialize(fields = empty, first = empty, second = empty)
+    alias Field = {Int32, Int32}
+
+    N = 3
+
+    VERTICAL = 1
+    HORIZONTAL = 0
+    ALIGNS = [VERTICAL, HORIZONTAL]
+
+    MAIN = -> (f : Field) { f[0] == f[1] }
+    BACK = -> (f : Field) { f[0] + f[1] == N - 1 }
+    DIAGONALS = [MAIN, BACK]
+
+    OUTCOMES = {
+      first_won: "First player won",
+      second_won: "Second player won",
+      draw: "Draw",
+    }
+
+    protected getter fields, first, second
+    def initialize(game : Array(String))
+      initialize
+
+      default = empty
+      dispatch = {
+        '_' => fields,
+        'x' => first,
+        'o' => second,
+      }
+
+      game.each_with_index do |line, i|
+        line.each_char.each_with_index do |char, j|
+          dispatch.fetch(char, default) << {i, j}
+        end
+      end
+    end
+
+    def initialize(@fields = empty : Array(Field), first = empty, second = empty, @notice = "")
+      @first = first.sort
+      @second = second.sort
+    end
+
+    macro from(*game)
+      {% game_table = game.map(&.stringify) %}
+      ::TicTacToe::Game.new({{game_table}})
     end
 
     def over?
+      won?(first) ||
+        won?(second) ||
+        fields.empty?
+    end
+
+    def take(field)
+      return self if over?
+
+      Take
+        .new(fields, first, second, field)
+        .next_game
+    end
+
+    def notice
+      @_notice ||= _notice
+    end
+
+    def outcome
+      return :ongoing unless over?
+
+      return :first_won if won?(first)
+      return :second_won if won?(second)
+
+      :draw
+    end
+
+    def ==(other : self)
+      self.fields == other.fields &&
+        self.first == other.first &&
+        self.second == other.second
+    end
+
+    private def full_column?(column)
+      (0...N).reduce(false) do |result, i|
+        result || column.first
+          .select(&.[column.last].== i)
+          .size == N
+      end
+    end
+
+    private def diagonal?(diag)
+      diag.first
+        .select(&diag.last)
+        .size == N
+    end
+
+    private def _notice
+      return over_notice if over?
+      @notice
+    end
+
+    private def won?(taken)
+      won_by_column?(taken) ||
+        won_by_diagonal?(taken)
+    end
+
+    private def won_by_column?(taken)
+      ALIGNS
+        .map { |align| full_column?({taken, align}) }
+        .any?
+    end
+
+    private def won_by_diagonal?(taken)
+      DIAGONALS
+        .map { |kind| diagonal?({taken, kind}) }
+        .any?
+    end
+
+    private def over_notice
+      "Game over. #{OUTCOMES[outcome]}."
     end
 
     private def empty
-      [] of {Int32, Int32}
+      [] of Field
+    end
+
+    private def self.empty
+      [] of Field
+    end
+
+    class Take
+      private getter fields, first, second, field
+      def initialize(@fields, @first, @second, @field)
+      end
+
+      def next_game
+        Game.new(
+          fields: take_field,
+          first: new_first,
+          second: new_second,
+          notice: new_notice,
+        )
+      end
+
+      private def take_field
+        @_take_field ||= fields.reject(&.== field)
+      end
+
+      private def new_first
+        new_takens.first
+      end
+
+      private def new_second
+        new_takens.last
+      end
+
+      private def new_notice
+        return "" if field_present?
+        "Field #{field} is already taken"
+      end
+
+      private def new_takens
+        @_new_takens ||= _new_takens
+      end
+
+      private def _new_takens
+        return nobody_taken unless field_present?
+        return second_taken unless first_takes?
+        first_taken
+      end
+
+      private def field_present?
+        take_field.size < fields.size
+      end
+
+      private def nobody_taken
+        {first, second}
+      end
+
+      private def first_taken
+        {first + [field], second}
+      end
+
+      private def second_taken
+        {first, second + [field]}
+      end
+
+      private def first_takes?
+        first.size == second.size
+      end
     end
   end
 end
